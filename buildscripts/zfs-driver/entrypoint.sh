d6e8b95b159e035a41218c60d0357b1446c388f1
Pawan <pawan@mayadata.io>
2020-05-24 22:01:28 +0530

    feat(centos): adding support for deploying ZFS-LocalPV on CentOS machine
    Since we mount the zfs host binary into the ZFS-LocalPV container,
    it needs to have all the comatible libraries available inside the
    container. With every relaese of Ubuntu, we have to change the docker
    base (ubuntu version) so that all the required library versions are
    available. As the newer version of library will be backward compatible,
    so this will work for old ubuntu version also. But there is a pain of
    changing the docker base with every release of ubuntu version.
    
    Approaching this issue in a different way and mounting the libraries
    (/lib and /lib64) into the conatiner and setting the LD_LIBRARY_PATH
    so that we search in that directory for the required libraries. So,
    with every ubuntu release, the new libraries will be mounted and we
    don't need to change the ubuntu base version with every release of it.
    
    Also, this way, we can add support for centos also, as we are mounting
    /lib and /lib64, the centos libraries will also be mounted. Here, as the
    zfs binrary path is different(/usr/sbin/zfs) in centos, we are mounting
    it with FileOrCreate, so that we can use the same yaml for the OS where
    the path is /sbin/zfs.
    
    Signed-off-by: Pawan <pawan@mayadata.io>

--- buildscripts/zfs-driver/entrypoint.sh
+++ buildscripts/zfs-driver/entrypoint.sh
@@ -1,5 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ex
 
-/usr/local/bin/zfs-driver
+# update the library cache
+ldconfig
+
+echo "[entrypoint.sh] launching zfs-driver."
+
+/usr/local/bin/zfs-driver "$@" &
+
+child=$!
+
+#sigterm caught SIGTERM signal and forward it to child process
+_sigterm() {
+  echo "[entrypoint.sh] caught SIGTERM signal forwarding to pid [$child]."
+  kill -TERM "$child" 2> /dev/null
+  waitForChildProcessToFinish
+}
+
+#sigint caught SIGINT signal and forward it to child process
+_sigint() {
+  echo "[entrypoint.sh] caught SIGINT signal forwarding to pid [$child]."
+  kill -INT "$child" 2> /dev/null
+  waitForChildProcessToFinish
+}
+
+#waitForChildProcessToFinish waits for child process to finish
+waitForChildProcessToFinish(){
+    while ps -p "$child" > /dev/null; do sleep 1; done;
+}
+
+trap _sigint INT
+trap _sigterm SIGTERM
+
+wait $child
