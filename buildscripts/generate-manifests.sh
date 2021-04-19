a3b6e734ed15bb7ab6fc956f8e6d2b5e4aeb3990
shubham <shubham.bajpai@mayadata.io>
2021-04-19 17:10:26 +0530

    refact(actions): use build & push action instead of makefile
      - update the generated crd changes in helm charts
      - add quay.io repo to push images
      - avoid build when release is created
    
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- buildscripts/generate-manifests.sh
+++ buildscripts/generate-manifests.sh
@@ -46,6 +46,7 @@ echo '
 
 cat deploy/yamls/zfs.openebs.io_zfsvolumes.yaml >> deploy/yamls/zfsvolume-crd.yaml
 rm deploy/yamls/zfs.openebs.io_zfsvolumes.yaml
+cp deploy/yamls/zfsvolume-crd.yaml deploy/helm/charts/crds/zfsvolume.yaml
 
 echo '
 
@@ -61,7 +62,7 @@ echo '
 
 cat deploy/yamls/zfs.openebs.io_zfssnapshots.yaml >> deploy/yamls/zfssnapshot-crd.yaml
 rm deploy/yamls/zfs.openebs.io_zfssnapshots.yaml
-
+cp deploy/yamls/zfssnapshot-crd.yaml deploy/helm/charts/crds/zfssnapshot.yaml
 
 echo '
 
@@ -77,7 +78,7 @@ echo '
 
 cat deploy/yamls/zfs.openebs.io_zfsbackups.yaml >> deploy/yamls/zfsbackup-crd.yaml
 rm deploy/yamls/zfs.openebs.io_zfsbackups.yaml
-
+cp deploy/yamls/zfsbackup-crd.yaml deploy/helm/charts/crds/zfsbackup.yaml
 
 echo '
 
@@ -93,6 +94,7 @@ echo '
 
 cat deploy/yamls/zfs.openebs.io_zfsrestores.yaml >> deploy/yamls/zfsrestore-crd.yaml
 rm deploy/yamls/zfs.openebs.io_zfsrestores.yaml
+cp deploy/yamls/zfsrestore-crd.yaml deploy/helm/charts/crds/zfsrestore.yaml
 
 ## create the operator file using all the yamls
 
