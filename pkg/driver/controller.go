137572552e1872517f3fe7e4f9d3a0d1a1813d31
Atibhi Agrawal <atibhi.a@gmail.com>
2021-05-06 17:07:43 +0530

    refact(error): handle errors (#326)
    Signed-off-by: aSquare14 <atibhi.a@gmail.com>

--- pkg/driver/controller.go
+++ pkg/driver/controller.go
@@ -288,6 +288,9 @@ func CreateVolClone(ctx context.Context, req *csi.CreateVolumeRequest, srcVol st
 		WithName(volName).
 		WithVolumeStatus(zfs.ZFSStatusPending).
 		WithLabels(labels).Build()
+	if err != nil {
+		return "", err
+	}
 
 	volObj.Spec = vol.Spec
 	// use the snapshot name same as new volname
@@ -342,6 +345,9 @@ func CreateSnapClone(ctx context.Context, req *csi.CreateVolumeRequest, snapshot
 		WithName(volName).
 		WithVolumeStatus(zfs.ZFSStatusPending).
 		Build()
+	if err != nil {
+		return "", err
+	}
 
 	volObj.Spec = snap.Spec
 	volObj.Spec.SnapName = strings.ToLower(snapshot)
