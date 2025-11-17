08269fb437a79397588bdefd5cf6dcf00f750549
Abhinandan Purkait <purkaitabhinandan@gmail.com>
2025-11-17 15:23:17 +0000

    chore: yq_ibl fix, fix registry to docker, fix release wf
    Signed-off-by: Abhinandan Purkait <purkaitabhinandan@gmail.com>

--- scripts/yq_utils.sh
+++ scripts/yq_utils.sh
@@ -2,13 +2,12 @@
 # this function gets around that using diff with --ignore-blank-lines
 yq_ibl()
 {
-  set +e
-  diff_out=$(diff -B <(yq '.' "$2") <(yq "$1" "$2")) || error=$?
+  error=0
+  diff_out=$(diff -B <(cat "$2") <(yq "$1" "$2")) || error=$?
   if [ "$error" != "0" ] && [ "$error" != "1" ]; then
     exit "$error"
   fi
   if [ -n "$diff_out" ]; then
     echo "$diff_out" | patch --quiet --no-backup-if-mismatch "$2" -
   fi
-  set -euo pipefail
 }
